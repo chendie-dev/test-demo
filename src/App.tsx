22bf6713ca39daea0a5b9b7ba3a86ad4e49a6d4b
chendie <chendie>
2024-09-20 11:01:39 +0800

    🎉 init(项目初始化):

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from './components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className="h-100 w-100 bg-red-600">
+        <Button>按钮</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
